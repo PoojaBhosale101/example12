f8b482097c06c7a297b6ebd914ad0d8c4ad791f4
PoojaBhosale101 <40861201+PoojaBhosale101@users.noreply.github.com>
2019-05-06 11:53:45 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$response = Unirest\Request::get("https://restcountries-v1.p.rapidapi.com/alpha/?codes=IN",
+  array(
+    "X-RapidAPI-Host" => "restcountries-v1.p.rapidapi.com",
+    "X-RapidAPI-Key" => "722e4771a0mshe4f7a8cd62ff53dp1fbb66jsn6a39b939e27a"
+  )
+);
+?>
